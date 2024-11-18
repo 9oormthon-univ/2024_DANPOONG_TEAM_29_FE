22d924eca6ab2a639218d7f1923f354a4cc9572d
nijuy <youjin6325@naver.com>
2024-11-18 23:18:20 +0900

    chore: 자주 쓰이는 컬러 tailwind.config에 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1A8CFF',
+        'light-gray': '#D9D9D9',
+        gray: '#838383',
+      },
+    },
   },
   plugins: [],
 };
