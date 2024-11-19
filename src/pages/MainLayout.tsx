be592654ad12976978de9caa17c77db3863b52bb
nijuy <youjin6325@naver.com>
2024-11-19 21:56:31 +0900

    refactor: MainLayout 컴포넌트에 overflow:scroll 추가
    - 콘텐츠 길이가 100vh를 넘어갈 경우 처리를 위함

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export const MainLayout = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-100">
-      <div className="relative h-full w-full max-w-[400px] rounded-lg bg-white p-6 shadow-md">
+    <div className="bg-gray-100 flex h-screen w-screen flex-col items-center justify-center">
+      <div className="relative h-full w-full max-w-[400px] overflow-scroll rounded-lg bg-white p-6 shadow-md">
         <Outlet />
       </div>
     </div>
