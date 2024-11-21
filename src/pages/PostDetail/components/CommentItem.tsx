92a886f9a5d4d9c4a54996bb0d49bba23bdf29d5
nijuy <youjin6325@naver.com>
2024-11-21 21:33:08 +0900

    feat: CommentItem 컴포넌트 생성

---
+++ src/pages/PostDetail/components/CommentItem.tsx
@@ -0,0 +1,29 @@
+import Spacing from '../../../components/Spacing';
+
+interface CommentItemProps {
+  profileImageUrl: string;
+  nickname: string;
+  content: string;
+  createdAt: string;
+}
+
+export const CommentItem = ({
+  profileImageUrl,
+  nickname,
+  content,
+  createdAt,
+}: CommentItemProps) => {
+  return (
+    <div className="flex">
+      <img src={profileImageUrl} className="h-10 w-10 rounded-full" alt="author image" />
+      <Spacing size={0.6} direction="horizontal" />
+      <div>
+        <div className="flex gap-2 leading-5">
+          <p className="grow text-xs font-medium">{nickname}</p>
+          <p className="grow text-xs text-[#939393]">{createdAt}</p>
+        </div>
+        <p className="grow text-xs">{content}</p>
+      </div>
+    </div>
+  );
+};
