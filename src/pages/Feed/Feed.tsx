2ec4c479057eb99abe1e01b9e0070cc36552de89
이유진 <youjin6325@naver.com>
2024-11-23 19:59:48 +0900

    Refactor: Feed 페이지 직종 필터와 정렬 필터 분리 (#22)
    * refactor: 정렬 순서용 타입과 직종 타입 분리
    
    * refactor: 피드 조회 API, 훅 수정
    
    - 정렬 순서와 직종을 분리
    
    * refactor: Feed 페이지에서 정렬 필터와 직종 필터 분리

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from 'react';
+
 import { useSearchParams } from 'react-router-dom';
 
+import LikeDescIcon from '@/assets/feed/filter_like.svg?react';
+import CreatedAtDescIcon from '@/assets/feed/filter_time.svg?react';
 import Spacing from '@/components/Spacing';
 import { useGetPostList, useGetRecommendUsers } from '@/hooks/queries/feed.query';
 import { useScrollObserve } from '@/hooks/useScrollObserve';
-import { FilterType } from '@/types/filterType';
+import { FilterType, SortType } from '@/types/filterType';
 import { PostItemType } from '@/types/postType';
 
 import { EmptyFeed } from './components/EmptyFeed';
@@ -12,10 +16,21 @@ import { PostItem } from './components/PostItem';
 import { RecommendUser } from './components/RecommendUser';
 
 export const Feed = () => {
-  const [searchParams] = useSearchParams();
-  const currentSortType = (searchParams.get('sortType') || 'CREATED_AT_DESC') as FilterType;
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
 
-  const { data: postList, isPending, fetchNextPage, hasNextPage } = useGetPostList(currentSortType);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentSortType = (searchParams.get('sortType') || 'CREATED_AT_DESC') as SortType;
+  const currentPartType = (searchParams.get('part') || '') as FilterType;
+
+  const {
+    data: postList,
+    isPending,
+    fetchNextPage,
+    hasNextPage,
+  } = useGetPostList({
+    currentSortType,
+    currentPartType,
+  });
   const { data: recommendUsers } = useGetRecommendUsers({});
 
   const allPostCount = (postList ? postList.pages.flat() : []).length;
@@ -26,11 +41,31 @@ export const Feed = () => {
     hasNextPage,
   });
 
+  useEffect(() => {
+    if (!searchParams.get('sortType')) {
+      setSearchParams({ sortType: 'CREATED_AT_DESC' });
+    }
+  }, [searchParams, setSearchParams]);
+
   return (
     <div>
-      <Filter />
+      <div className="flex items-center justify-end">
+        {isFilterVisible == false && (
+          <div className="ml-2 flex gap-2">
+            <CreatedAtDescIcon
+              className={currentSortType === 'CREATED_AT_DESC' ? 'text-primary' : 'text-[#979797]'}
+              onClick={() => setSearchParams({ sortType: 'CREATED_AT_DESC' })}
+            />
+            <LikeDescIcon
+              className={currentSortType === 'LIKE_DESC' ? 'text-primary' : 'text-[#979797]'}
+              onClick={() => setSearchParams({ sortType: 'LIKE_DESC' })}
+            />
+          </div>
+        )}
+        <Filter isFilterVisible={isFilterVisible} setIsFilterVisible={setIsFilterVisible} />
+      </div>
       {allPostCount === 0 ? (
-        <EmptyFeed sortType={currentSortType} />
+        <EmptyFeed filterType={currentPartType} />
       ) : (
         <div>
           {postList.pages.flatMap((page) =>
