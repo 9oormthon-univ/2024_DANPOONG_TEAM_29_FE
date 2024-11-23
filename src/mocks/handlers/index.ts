d77efee85cd84f7265d80cff9dac7425fd34edf8
nijuy <youjin6325@naver.com>
2024-11-23 20:40:30 +0900

    feat: 이야기 게시용 모킹 핸들러 구현

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,6 +1,6 @@
 import { feedHandlers } from './feed';
-
+import { postHandlers } from './post';
 /**
  * feed.ts에서 작성한 feedHandlers를 handlers 배열에 삽입합니다.
  */
-export const handlers = [...feedHandlers];
+export const handlers = [...feedHandlers, ...postHandlers];
